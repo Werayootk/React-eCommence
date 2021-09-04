8c78a43d66e61fb16816ef6a0e2fd451b04480ef
Werayootk <werayoot5800@gmail.com>
2021-09-04 20:25:51 +0700

    Add Project

---
+++ build/precache-manifest.f67c8bf0e986c525a5a556d96fa413af.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09ec1da56f55a404e87ac93f98930d84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "539588c1390236700d19",
+    "url": "/static/css/main.ff56beb9.chunk.css"
+  },
+  {
+    "revision": "40e5534ec260256bc49f",
+    "url": "/static/js/2.96217c0a.chunk.js"
+  },
+  {
+    "revision": "dcd1d80362aef046a5450470cca1218a",
+    "url": "/static/js/2.96217c0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "539588c1390236700d19",
+    "url": "/static/js/main.8c5ae73b.chunk.js"
+  },
+  {
+    "revision": "b14ea0db6cdb287a0925",
+    "url": "/static/js/runtime-main.f8c9dd52.js"
+  },
+  {
+    "revision": "19190d053a8b13a3d9f528c9f7628817",
+    "url": "/static/media/shopMens.19190d05.jpg"
+  },
+  {
+    "revision": "c9c60ae5dd1c192f2617996f1b693582",
+    "url": "/static/media/shopWomens.c9c60ae5.jpg"
+  }
+]);
